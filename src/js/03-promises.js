a293554eed1e23b4a71ced5ad4487be07e4e73cd
Viktoriia Slobodeniuk <vikavas988@gmail.com>
2023-01-17 01:27:33 +0200

    promises done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const firstDelayInput = document.querySelector('input[name="delay"]');
+const stepInput = document.querySelector('input[name="step"]');
+const amountInput = document.querySelector('input[name="amount"]');
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', e => {
+  e.preventDefault();
+  const firstDelay = Number(firstDelayInput.value);
+  const step = Number(stepInput.value);
+  const amount = Number(amountInput.value);
+
+  for (let position = 1; position <= amount; position += 1) {
+    const delay = firstDelay + step * (position - 1);
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+});
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      const result = { position, delay };
+      if (shouldResolve) {
+        return resolve(result);
+      } else {
+        return reject(result);
+      }
+    }, delay);
+  });
 }
